18ae63cf61fc55aa053f54a86dd90c29e985e190
Usmon <usmonhakimov1999@gmail.com>
2022-05-10 21:34:37 +0900

    add responsiveness

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const elnavOpen = document.body;
+const elOverflow = document.querySelector(".js-overflow");
+const elHamburger = document.querySelector(".hamburger");
+const elNavList = document.querySelector(".header__nav-list");
+
+const openFunction = () => {
+  elnavOpen.classList.toggle("nav-open");
+  elOverflow.classList.toggle("overflow");
+  elHamburger.classList.toggle("hamburger-active");
+  elNavList.classList.toggle("js-nav-list-open");
+};
+
+elHamburger.addEventListener("click", openFunction);
